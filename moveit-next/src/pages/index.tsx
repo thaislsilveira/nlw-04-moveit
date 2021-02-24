f0142b70187324eafbffe0a8ddde74a49b1a03e7
Thaís Silveira <thais_lsilveira@hotmail.com>
2021-02-24 14:04:11 -0300

    feat: add context and components

--- moveit-next/src/pages/index.tsx
+++ moveit-next/src/pages/index.tsx
@@ -2,6 +2,7 @@ import { ExperienceBar } from '../components/ExperienceBar';
 import { Profile } from '../components/Profile';
 import { CompletedChallenges } from '../components/CompletedChallenges';
 import { Countdown } from '../components/Countdown';
+import { ChallengeBox } from '../components/ChallengeBox';
 
 import Head from 'next/head';
 
@@ -21,7 +22,9 @@ export default function Home() {
           <CompletedChallenges />
           <Countdown />
         </div>
-        <div></div>
+        <div>
+        <ChallengeBox />
+        </div>
       </section>
     </div>
   );
